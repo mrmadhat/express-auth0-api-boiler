712407810c1c68798cea87f3bd2969428e6fe287
mrmadhat <danny@websitesbydanny.co.uk>
2020-01-05 18:20:20 +0000

    add 404 handler and move middleware registration location

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,6 +1,8 @@
 const customErrorMessage = require("../utils/customErrorMessage")
 
 module.exports = (err, req, res, next) => {
-  customErrorMessage(err)
-  res.status(401).send({ error: "not authorised" })
+  if (err) {
+    customErrorMessage(err)
+    res.status(401).send({ error: "not authorised" })
+  }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 const protectRoute = require("../middleware/protect-route")
+const errorHandler = require("../middleware/error-handler")
+const unknownRouteHandler = require("../middleware/unknown-route-handler")
+
 const v1Routes = require("./v1")
 
 module.exports = app => {
   app.use("/v1", protectRoute, v1Routes)
+  app.use(errorHandler)
+  app.use(unknownRouteHandler)
 }
